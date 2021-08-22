58309823c68865825662e2a0a6724d5f1f6051dd
Cris John Alonzaga <cjalonzaga@gmail.com>
2021-08-22 06:30:41 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var i = 0;
+var txt = "Hi I'm Cris";
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("type-writer").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, 200);
+  }
+}
+
+window.addEventListener("load", function(){
+  	typeWriter();
+});
+
+function addClass(){
+	var dom = document.getElementsByClassName('inner-banner-content');
+	dom[0].setAttribute('class', 'inner-banner-content addsize');
+}
+setTimeout(addClass() , 50);
+
+var toggler = document.getElementById('toggler');
+
+toggler.addEventListener('click', function(){
+	document.querySelector('.nav-menu').style.display = 'flex';
+	document.querySelector('#toggler').style.display = 'none';
+});
+
+var close_toggler = document.getElementById('close-toggler');
+close_toggler.addEventListener('click', function(){
+	document.querySelector('.nav-menu').style.display = 'none';
+	document.querySelector('#toggler').style.display = 'block';
+});
